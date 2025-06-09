24f567c567fd3f24401bf4a3dcf2f293608d9016
Bino <153579759+bino1423@users.noreply.github.com>
2025-06-09 15:17:30 +0530

    Add files via upload

---
+++ Employees.java
@@ -0,0 +1,29 @@
+package programs;
+
+public class Employees {
+
+	public void salary(String work,double salary) {
+		System.out.println("Your work is: "+work);
+		System.out.println("Your salaey is: "+salary);
+	}
+	
+	public void salary(String work,double salary,int time) {
+		System.out.println("Your work is: "+work);
+		
+		System.out.println("Your salary is: "+(salary*time));
+	}
+	public void salary(String work,double salary,int time,float bonus) {
+		System.out.println("Your work is: "+work);
+		
+		System.out.println("Your salary is: "+(salary*time*bonus));
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employees e=new Employees();
+		e.salary("Full-Time", 10000);
+		e.salary("Part-Time", 1000,5);
+		e.salary("FreeLancer", 1000,4,2);
+	}
+
+}
